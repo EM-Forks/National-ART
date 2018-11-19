d6c71eecfbd189a5b4a0fb9d03a9dc56f9594921
Ernest Matola <mangochiman@gmail.com>
2018-11-19 11:54:46 +0200

    Removed limit in migration

--- db/migrate/20160112152154_create_drug_cms.rb
+++ db/migrate/20160112152154_create_drug_cms.rb
@@ -4,15 +4,15 @@ class CreateDrugCms < ActiveRecord::Migration[5.2]
       t.integer   :drug_inventory_id, :null => false
       t.string    :name, :null => false
       t.string    :code
-      t.string    :short_name, :limit => 225
-      t.string    :tabs, :limit => 225
+      t.string    :short_name
+      t.string    :tabs
       t.integer   :pack_size
       t.integer   :weight
       t.string    :strength
-      t.integer   :voided, :default => 0, :limit => 1
-      t.integer   :voided_by , :limit => 11
+      t.integer   :voided, :default => 0
+      t.integer   :voided_by
       t.datetime  :date_voided
-      t.string    :void_reason, :limit => 225
+      t.string    :void_reason
       t.timestamps
     end
     execute "ALTER TABLE `drug_cms` CHANGE COLUMN `drug_inventory_id` `drug_inventory_id` INT(11) NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`drug_inventory_id`);"
